cb9c127d67927ba67cb5701a9b44d2c670fbe5fe
gmidolo <gabriele.midolo@gmail.com>
2025-07-06 14:38:10 +0200

    update script fig 4

--- src/4_plot_figures/mapping_perc_change.R
+++ src/4_plot_figures/mapping_perc_change.R
@@ -18,6 +18,7 @@ suppressPackageStartupMessages({
 # Load predictions at the plot level
 dat <- './data/preds/preds_stdpltsz.rf.csv' %>%
  read_csv(show_col_types = F) %>%
+ # restrict the analyses to plot within the studied period
  filter(year >= 1960 & year <= 2020)
 
 # Load EU shapefile         
@@ -110,9 +111,9 @@ map <- ggplot() +
  labs(fill='Percentage change') +
  geom_rect(data = subset(d2plot_refined, period %in% c('From 1960 to 2020')), 
                           linewidth = 1.25, fill = NA, colour = 'grey20', xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
- my_theme +
- theme(legend.position = 'bottom') +
- guides(fill = guide_legend(reverse=TRUE, direction = 'horizontal', nrow = 1))
+ my_theme #+
+ # theme(legend.position = 'bottom') +
+ # guides(fill = guide_legend(reverse=TRUE, direction = 'horizontal', nrow = 1))
 map
 
 # Export
@@ -168,15 +169,15 @@ for (h in c('Forest', 'Grassland', 'Scrub', 'Wetland')) {
   }
 }
 
-# Export histograms
-for (h in names(hists)) {
-
-  ggsave(
-    paste0('./fig/S_trends/hist_', h, '.svg'),
-    cowplot::plot_grid(hists[[h]][[1]], hists[[h]][[2]], hists[[h]][[3]], hists[[h]][[4]], labels = c('1960.1980', '1980.2000', '1980.2000', '2000.2020')),
-    width = 5 * 0.9,
-    height = 4 * 0.9
-  )
-  
-}
+# # Export histograms
+# for (h in names(hists)) {
+# 
+#   ggsave(
+#     paste0('./fig/S_trends/hist_', h, '.svg'),
+#     cowplot::plot_grid(hists[[h]][[1]], hists[[h]][[2]], hists[[h]][[3]], hists[[h]][[4]], labels = c('1960.1980', '1980.2000', '1980.2000', '2000.2020')),
+#     width = 5 * 0.9,
+#     height = 4 * 0.9
+#   )
+#   
+# }
 
